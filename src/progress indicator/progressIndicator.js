9ea7ca9eb2e2c98c468cf53c071e0e092f3cf352
Karim Naser <karim.naser@elabdfoods.com>
2024-11-25 14:40:28 +0200

    adding border color to active class

--- src/progress indicator/progressIndicator.js
+++ src/progress indicator/progressIndicator.js
@@ -1,15 +1,14 @@
 export default function progressIndicator(order) {
     const states = document.querySelectorAll('.state');
     if (order) {
-        states.forEach(state => state.classList.remove('active'));
         states.forEach(state => {
             if (state.dataset.order == order) {
-                state.classList.add('active')
+                state.classList.add('active');
             }
         })
     }
     return `
-        <span data-order="1" class="active state">1</span>
+        <span data-order="1" class="state">1</span>
         <span data-order="2" class="state">2</span>
         <span data-order="3" class="state">3</span>
         <span data-order="4" class="state">4</span>`
